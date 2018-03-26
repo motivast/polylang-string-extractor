0805834ed2e28ded38f045d3cfdc0bc671210947
Piotr Kierzniewski <p.kierzniewski@viewone.pl>
2018-03-26 15:51:46 +0200

    Add support for scaning plugins also

--- inc/class-scanner.php
+++ inc/class-scanner.php
@@ -12,6 +12,8 @@
 
 namespace PolylangStringExtractor;
 
+use Gettext\Translations;
+
 use PolylangStringExtractor\Extractors\WPCode;
 
 /**
@@ -37,44 +39,150 @@ class Scanner {
 	/**
 	 * Scan files
 	 *
-	 * Scan all files in activated theme and activated directory.
+	 * Scan all files in current theme and activated plugins for translations.
 	 */
 	public function scan() {
 
-		$strings = array();
-		$theme_name = get_template();
-		$theme_dir = get_template_directory();
+		$theme_strings   = $this->scan_current_theme();
+		$plugins_strings = $this->scan_active_plugins();
 
-		// http://php.net/manual/en/class.recursivedirectoryiterator.php#97228.
-		$directory = new \RecursiveDirectoryIterator( $theme_dir );
-		$iterator = new \RecursiveIteratorIterator( $directory );
-
-		$regex = new \RegexIterator( $iterator, '/^.+\.php$/i', \RecursiveRegexIterator::GET_MATCH );
-
-		$translations = new \Gettext\Translations();
-		$translations->setDomain( $theme_name );
-
-		foreach ( $regex as $files ) {
-			$path = current( $files );
-
-			WPCode::fromFile( $path, $translations );
-		}
-
-		$translations_iterator = $translations->getIterator();
-
-		foreach ( $translations_iterator as $translation ) {
-			$string = $translation->getOriginal();
-
-			$strings[ md5( $string ) ] = array(
-				'name' => '',
-				'string' => $string,
-				'context' => $theme_name,
-				'multiline' => false,
-			);
-		}
+		$strings = array_merge( $theme_strings, $plugins_strings );
 
 		$option_name = sprintf( '%s_strings', $this->plugin['id'] );
 
 		update_option( $option_name , $strings );
+	}
+
+	/**
+	 * Scan current theme
+	 *
+	 * Scan all php files in current theme for translations.
+	 *
+	 * @return array Array of strings to translate
+	 */
+	private function scan_current_theme() {
+
+		$theme = wp_get_theme();
+
+		if ( ! ( $theme instanceof \WP_Theme ) ) {
+			return array();
+		}
+
+		$textdomain = $theme->get( 'TextDomain' );
+
+		if ( ! $textdomain ) {
+			return array();
+		}
+
+		$theme_dir = get_template_directory();
+
+		$php_files = $this->get_php_files( $theme_dir );
+
+		$translations = new Translations();
+		$translations->setDomain( $textdomain );
+
+		foreach ( $php_files as $file ) {
+			WPCode::fromFile( $file, $translations );
+		}
+
+		return $this->format_to_polylang( $translations );
+	}
+
+	/**
+	 * Scan activated plugins
+	 *
+	 * Scan all php files in activated plugins for translations.
+	 *
+	 * @return array Array of strings to translate
+	 */
+	private function scan_active_plugins() {
+
+		$all_translations = array();
+
+		$plugins_dir = WP_PLUGIN_DIR;
+		$active_plugins = get_option( 'active_plugins' );
+
+		foreach ( $active_plugins as $plugin ) {
+
+			$plugin_path = $plugins_dir . DIRECTORY_SEPARATOR . $plugin;
+			$plugin_info = get_plugin_data( $plugin_path );
+
+			if ( isset( $plugin_info['TextDomain'] ) && ! empty( $plugin_info['TextDomain'] ) ) {
+
+				$php_files = $this->get_php_files( plugin_dir_path( $plugin_path ) );
+
+				$translations = new Translations();
+				$translations->setDomain( esc_html( $plugin_info['TextDomain'] ) );
+
+				foreach ( $php_files as $file ) {
+					WPCode::fromFile( $file, $translations );
+				}
+
+				$all_translations = array_merge( $all_translations, $this->format_to_polylang( $translations ) );
+			}
+		}
+
+		return $all_translations;
+	}
+
+	/**
+	 * Get array of php files from path
+	 *
+	 * @param string $path Path to looking for.
+	 *
+	 * @return array Array of php files
+	 */
+	private function get_php_files( $path ) {
+
+		$flattened = array();
+
+		// http://php.net/manual/en/class.recursivedirectoryiterator.php#97228.
+		$directory = new \RecursiveDirectoryIterator( $path );
+		$iterator = new \RecursiveIteratorIterator( $directory );
+
+		$regex = new \RegexIterator( $iterator, '/^.+\.php$/i', \RecursiveRegexIterator::GET_MATCH );
+
+		/**
+		 * Flatten multidimensional array from iterator
+		 */
+		array_walk_recursive( iterator_to_array( $regex ), function( $file ) use ( &$flattened ) {
+			$flattened[] = $file;
+		} );
+
+		return $flattened;
+	}
+
+	/**
+	 * Format translations
+	 *
+	 * Format translations object to polylang translation array
+	 *
+	 * @param Translations $translations Gettext translations object.
+	 *
+	 * @return array Array of translations in polylang format
+	 */
+	private function format_to_polylang( $translations ) {
+
+		$strings = array();
+
+		foreach ( $translations as $translation ) {
+
+			$string = $translation->getOriginal();
+			$domain = $translations->getDomain();
+
+			$strings[ md5( $string ) ] = array(
+				'name' => '',
+				'string' => $string,
+
+				/**
+				 * Polylang use context value as group name in administration.
+				 * This should be obviously called domain or textdomain.
+				 */
+				'context' => $domain,
+				'multiline' => false,
+			);
+		}
+
+		return $strings;
 	}
 }
